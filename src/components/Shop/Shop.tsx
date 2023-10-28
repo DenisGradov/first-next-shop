424a4a4306c12a8c420db8c57650fb6a57ecd6fc
Stenford <varonapika@gmail.com>
2023-10-28 13:15:08 +0200

    Фікс багів: При портуванні інші технології збилися css властивості. Це було виправлено

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -12,8 +12,6 @@ import UnstyledSelectBasic from "../UnstyledSelectBasic";
 import UnstyledButtonsSimple from "../UnstyledButtonsSimple";
 import AccordionIndeterminateCheckbox from "../AccordionIndeterminateCheckbox";
 import PaginationOutlined from "../PaginationOutlined";
-import UnstyledButtonsSimple2 from "../UnstyledButtonsSimple2";
-import UnstyledButtonsSimple3 from "../UnstyledButtonsSimple3";
 
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Product } from "@/types/types";
@@ -53,134 +51,46 @@ export default function Shop() {
       <div className={styles.itemBox}>
         {!!product.productsWithPagination.length && (
           <>
-            {windowSize < 1118
-              ? true && (
-                  <div className={styles.shopFiltersBlock}>
-                    <Accordion
-                      style={{
-                        padding: "5px",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        margin: "0 auto",
-                        width: "76vw",
-                        zIndex: "100",
-                        overflowY: "auto",
-                      }}
-                      className={styles.Fil}
-                    >
-                      <AccordionSummary
-                        style={{
-                          zIndex: "999",
-                        }}
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                      >
-                        <Typography component="div">Фільтри</Typography>
-                      </AccordionSummary>
-                      <div
-                        style={{
-                          maxHeight: "400px",
-                          margin: "0 auto",
-                          height: "300px",
-                        }}
-                      >
-                        <AccordionIndeterminateCheckbox />
-                        <div
-                          className={styles.priceRange}
-                          style={{
-                            zIndex: "999",
-                            height: "100px",
-                          }}
-                        >
-                          <Slider
-                            valueLabelDisplay="off"
-                            className={styles.priceRangeSlider}
-                            getAriaLabel={() => "Temperature range"}
-                            value={minMaxPrice.values[1]}
-                            min={minMaxPrice.values[0][0]}
-                            max={minMaxPrice.values[0][1]}
-                            step={
-                              (minMaxPrice.values[0][1] -
-                                minMaxPrice.values[0][0]) /
-                              100
-                            }
-                            onChange={(e, newValue) => {
-                              const newMinMaxPrice: [number[], number[]] = [
-                                minMaxPrice.values[0],
-                                newValue as number[],
-                              ];
-                              minMaxPrice.setAll(newMinMaxPrice);
-                            }}
-                            style={{
-                              zIndex: "999",
-                            }}
-                          />
-                          <div className={styles.priceRangeIputs}>
-                            <input
-                              style={{
-                                zIndex: "999",
-                              }}
-                              onChange={(e) => {
-                                if (
-                                  typeof parseFloat(e.target.value) != "number"
-                                )
-                                  return;
-                                const valueFromEvent: number = parseFloat(
-                                  e.target.value
-                                );
-                                if (
-                                  valueFromEvent > minMaxPrice.values[0][0] &&
-                                  valueFromEvent < minMaxPrice.values[0][1]
-                                ) {
-                                  const newMinMaxPrice = { ...minMaxPrice };
-                                  newMinMaxPrice.values[1][0] = valueFromEvent;
-                                  minMaxPrice.setAll(newMinMaxPrice.values);
-                                }
-                              }}
-                              value={minMaxPrice.values[1][0]}
-                              className={styles.priceRangeInput}
-                            />
-                            <input
-                              onChange={(e) => {
-                                if (
-                                  typeof parseFloat(e.target.value) != "number"
-                                )
-                                  return;
-                                const valueFromEvent: number = parseFloat(
-                                  e.target.value
-                                );
-                                if (
-                                  valueFromEvent > minMaxPrice.values[0][0] &&
-                                  valueFromEvent < minMaxPrice.values[0][1]
-                                ) {
-                                  const newMinMaxPrice = { ...minMaxPrice };
-                                  newMinMaxPrice.values[1][0] = valueFromEvent;
-                                  minMaxPrice.setAll(newMinMaxPrice.values);
-                                }
-                              }}
-                              value={minMaxPrice.values[1][1]}
-                              className={styles.priceRangeInput}
-                            />
-                          </div>
-                          <div className={styles.priceRangeMinMax}>
-                            <h2 className={styles.priceRangeText}>
-                              {minMaxPrice.values[0][0]}$
-                            </h2>
-                            <h2 className={styles.priceRangeText}>
-                              {minMaxPrice.values[0][1]}$
-                            </h2>
-                          </div>
-                        </div>
-                      </div>
-                    </Accordion>
-                  </div>
-                )
-              : true && (
-                  <div>
+            {windowSize < 1118 ? (
+              <div className={styles.shopFiltersBlock}>
+                <Accordion
+                  style={{
+                    padding: "5px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    margin: "0 auto",
+                    width: "76vw",
+                    zIndex: "100",
+                    overflowY: "auto",
+                  }}
+                  className={styles.Fil}
+                >
+                  <AccordionSummary
+                    style={{
+                      zIndex: "999",
+                    }}
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography component="div">Фільтри</Typography>
+                  </AccordionSummary>
+                  <div
+                    style={{
+                      maxHeight: "400px",
+                      margin: "0 auto",
+                      height: "300px",
+                    }}
+                  >
                     <AccordionIndeterminateCheckbox />
-                    <div className={styles.priceRange}>
+                    <div
+                      className={styles.priceRange}
+                      style={{
+                        zIndex: "999",
+                        height: "100px",
+                      }}
+                    >
                       <Slider
                         valueLabelDisplay="off"
                         className={styles.priceRangeSlider}
@@ -195,14 +105,20 @@ export default function Shop() {
                         }
                         onChange={(e, newValue) => {
                           const newMinMaxPrice: [number[], number[]] = [
-                            minMaxPrice.values[1],
+                            minMaxPrice.values[0],
                             newValue as number[],
                           ];
                           minMaxPrice.setAll(newMinMaxPrice);
                         }}
+                        style={{
+                          zIndex: "999",
+                        }}
                       />
                       <div className={styles.priceRangeIputs}>
                         <input
+                          style={{
+                            zIndex: "999",
+                          }}
                           onChange={(e) => {
                             if (typeof parseFloat(e.target.value) != "number")
                               return;
@@ -233,7 +149,7 @@ export default function Shop() {
                               valueFromEvent < minMaxPrice.values[0][1]
                             ) {
                               const newMinMaxPrice = { ...minMaxPrice };
-                              newMinMaxPrice.values[1][1] = valueFromEvent;
+                              newMinMaxPrice.values[1][0] = valueFromEvent;
                               minMaxPrice.setAll(newMinMaxPrice.values);
                             }
                           }}
@@ -251,10 +167,85 @@ export default function Shop() {
                       </div>
                     </div>
                   </div>
-                )}
+                </Accordion>
+              </div>
+            ) : (
+              <div className={styles.filters}>
+                <AccordionIndeterminateCheckbox />
+                <div className={styles.priceRange}>
+                  <Slider
+                    valueLabelDisplay="off"
+                    className={styles.priceRangeSlider}
+                    getAriaLabel={() => "Temperature range"}
+                    value={minMaxPrice.values[1]}
+                    min={minMaxPrice.values[0][0]}
+                    max={minMaxPrice.values[0][1]}
+                    step={
+                      (minMaxPrice.values[0][1] - minMaxPrice.values[0][0]) /
+                      100
+                    }
+                    onChange={(e, newValue) => {
+                      const newMinMaxPrice: [number[], number[]] = [
+                        minMaxPrice.values[1],
+                        newValue as number[],
+                      ];
+                      minMaxPrice.setAll(newMinMaxPrice);
+                    }}
+                  />
+                  <div className={styles.priceRangeIputs}>
+                    <input
+                      onChange={(e) => {
+                        if (typeof parseFloat(e.target.value) != "number")
+                          return;
+                        const valueFromEvent: number = parseFloat(
+                          e.target.value
+                        );
+                        if (
+                          valueFromEvent > minMaxPrice.values[0][0] &&
+                          valueFromEvent < minMaxPrice.values[0][1]
+                        ) {
+                          const newMinMaxPrice = { ...minMaxPrice };
+                          newMinMaxPrice.values[1][0] = valueFromEvent;
+                          minMaxPrice.setAll(newMinMaxPrice.values);
+                        }
+                      }}
+                      value={minMaxPrice.values[1][0]}
+                      className={styles.priceRangeInput}
+                    />
+                    <input
+                      onChange={(e) => {
+                        if (typeof parseFloat(e.target.value) != "number")
+                          return;
+                        const valueFromEvent: number = parseFloat(
+                          e.target.value
+                        );
+                        if (
+                          valueFromEvent > minMaxPrice.values[0][0] &&
+                          valueFromEvent < minMaxPrice.values[0][1]
+                        ) {
+                          const newMinMaxPrice = { ...minMaxPrice };
+                          newMinMaxPrice.values[1][1] = valueFromEvent;
+                          minMaxPrice.setAll(newMinMaxPrice.values);
+                        }
+                      }}
+                      value={minMaxPrice.values[1][1]}
+                      className={styles.priceRangeInput}
+                    />
+                  </div>
+                  <div className={styles.priceRangeMinMax}>
+                    <h2 className={styles.priceRangeText}>
+                      {minMaxPrice.values[0][0]}$
+                    </h2>
+                    <h2 className={styles.priceRangeText}>
+                      {minMaxPrice.values[0][1]}$
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            )}
           </>
         )}
-        {true ? (
+        {
           <div className={styles.shopItems}>
             {!!!product.productsWithPagination.length && (
               <Box
@@ -271,32 +262,25 @@ export default function Shop() {
             )}
             {!!product.productsWithPagination.length &&
               product.productsWithPagination[0] && (
-                <div className={styles.shopItemsProducts}>
-                  <div className={styles.shopItemsProductsBlock}>
-                    <div className={styles.shopItemsProductsBlockItems}>
-                      {product.productsWithPagination.map(
-                        (productsArray, page) => {
-                          if (page == product.activePage) {
-                            return productsArray.map((product: Product) => {
-                              return (
-                                <ProductItem
-                                  key={product.id}
-                                  product={product}
-                                />
-                              );
-                            });
-                          }
+                <>
+                  <div className={styles.shopItemsProducts}>
+                    {product.productsWithPagination.map(
+                      (productsArray, page) => {
+                        if (page == product.activePage) {
+                          return productsArray.map((product: Product) => {
+                            return (
+                              <ProductItem key={product.id} product={product} />
+                            );
+                          });
                         }
-                      )}
-                    </div>
-                    <PaginationOutlined />
+                      }
+                    )}
                   </div>
-                </div>
+                  <PaginationOutlined />
+                </>
               )}
           </div>
-        ) : (
-          <div className={styles.shopItems}></div>
-        )}
+        }
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,8 @@
 "use client";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
-const inter = Inter({ subsets: ["latin"] });
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import useData from "@/hooks/useData";
-import usePagination from "@/hooks/usePagination";
 import Shop from "@/components/Shop/Shop";
-import { Product, ProductInfo } from "@/types/types";
 import { useProduct } from "@/states/productInfo";
-import { useMinMaxPrice } from "@/states/minMaxPrice";
-import { useFilters } from "@/states/filters";
-import { useSearchInput } from "@/states/searchInput";
-import { useSortHow } from "@/states/sortHow";
-import { fetchData } from "@/api/api";
 
 export default function Home() {
   const product = useProduct();
